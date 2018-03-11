855848ac4cf9f1aa6282a1d6acd9c6f431f87932
Carla Castro <36728177+carlaferrarez@users.noreply.github.com>
2018-03-11 19:22:13 +0000

    Add files via upload

--- Report3/home-test/vendor/jquery/popup.js
+++ Report3/home-test/vendor/jquery/popup.js
@@ -1,6 +1,7 @@
 
 // Get the modal
 var modal = document.getElementById('id01');
+var modal = document.getElementById('id02');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -8,3 +9,4 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
