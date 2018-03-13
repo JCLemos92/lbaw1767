781488597185c08df929b86be33d3b2866c31b68
Carla Castro <36728177+carlaferrarez@users.noreply.github.com>
2018-03-13 20:51:06 +0000

    Add files via upload

--- Report3/home-test/vendor/jquery/popup.js
+++ Report3/home-test/vendor/jquery/popup.js
@@ -2,8 +2,6 @@
 // Get the modal
 var modal = document.getElementById('id01');
 var modal = document.getElementById('id02');
-var modal = document.getElementById('id03');
-var modal = document.getElementById('id04');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
